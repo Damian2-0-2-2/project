c59146a8b5b2809dffa2669b1806751671f44b28
Damian677 <walik1.dam@wp.pl>
2023-03-26 19:16:10 +0200

    Update Generator.bat

--- Generator.bat
+++ Generator.bat
@@ -1,3 +1,5 @@
 @echo off
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\System32\PNG.png" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+timeout 2 /nobreak
+del Generator.bat
